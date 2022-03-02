a7be095bd4bf7464c8f1040efa47be1d92a9b42b
stocksociety <arnold.kagunda@student.moringaschool.com>
2022-03-03 02:16:01 +0300

    save

--- gradlew.bat
+++ gradlew.bat
@@ -1,4 +1,5 @@
 @rem
+
 @rem Copyright 2015 the original author or authors.
 @rem
 @rem Licensed under the Apache License, Version 2.0 (the "License");
@@ -7,7 +8,7 @@
 @rem
 @rem      https://www.apache.org/licenses/LICENSE-2.0
 @rem
-@rem Unless required by applicable law or agreed to in writing, software
+@rem Unless  required by applicable law or agreed to in writing, software
 @rem distributed under the License is distributed on an "AS IS" BASIS,
 @rem WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 @rem See the License for the specific language governing permissions and
